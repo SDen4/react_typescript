d3ef8be7bcce92bf0cbcaef61bb60f4354a28813
SDen4 <asw325@yandex.ru>
2021-04-08 11:44:20 +0500

    TodoForm component

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+export const TodoForm: React.FC = () => {
+  const [title, setTitle] = useState<string>('');
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    console.log(title);
+  };
+
+  const keyPressHandler = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      console.log(title);
+      setTitle('');
+    }
+  };
+
+  return (
+    <div className="input-field mt2">
+      <input
+        value={title}
+        type="text"
+        id="title"
+        placeholder="Enter todo"
+        onChange={changeHandler}
+        onKeyPress={keyPressHandler}
+      />
+      <label htmlFor="title" className="active">
+        Enter todo
+      </label>
+    </div>
+  );
+};

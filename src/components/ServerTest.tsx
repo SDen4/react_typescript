9ee6b644b71238061cb29472e34b5c1b3fcaab21
SDen4 <asw325@yandex.ru>
2021-04-11 21:12:04 +0500

    get todos form server

--- src/components/ServerTest.tsx
+++ src/components/ServerTest.tsx
@@ -2,13 +2,39 @@ import React from 'react';
 import axios from 'axios';
 
 export default class ServerTest extends React.Component {
-  componentDidMount() {
-    axios
-      .get('https://react-ts-2c869-default-rtdb.firebaseio.com/test.json')
-      .then((response) => console.log(response));
+  state = {
+    dataFromServer: [],
+  };
+
+  async componentDidMount() {
+    try {
+      await axios
+        .get('https://react-ts-2c869-default-rtdb.firebaseio.com/todos.json')
+        .then((response) => {
+          this.setState(() => ({
+            dataFromServer: Object.values(response.data),
+          }));
+        });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   render() {
-    return <h1>Server Test</h1>;
+    const data = this.state.dataFromServer;
+    return (
+      <>
+        <h1>Server Test</h1>
+        {data.map((item) => {
+          const { id, title, completed } = item;
+          return (
+            <div key={id}>
+              <h5>{title}</h5>
+              <span>Completed: {completed ? 'true' : 'false'}</span>
+            </div>
+          );
+        })}
+      </>
+    );
   }
 }

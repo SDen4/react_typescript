2090e87bc808ead4ade97f014134e403c78401ab
SDen4 <asw325@yandex.ru>
2021-04-11 16:50:26 +0500

    test request to server

--- src/components/ServerTest.tsx
+++ src/components/ServerTest.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class ServerTest extends React.Component {
+  componentDidMount() {
+    axios
+      .get('https://react-ts-2c869-default-rtdb.firebaseio.com/test.json')
+      .then((response) => console.log(response));
+  }
+
   render() {
     return <h1>Server Test</h1>;
   }

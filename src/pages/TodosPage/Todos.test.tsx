57d22d564f24e396fb895458af70a4cd505116ff
SDen4 <asw325@yandex.ru>
2021-04-14 10:54:44 +0500

    yarn add @types/enzyme-adapter-react-16 --dev

--- src/pages/TodosPage/Todos.test.tsx
+++ src/pages/TodosPage/Todos.test.tsx
@@ -1,6 +1,7 @@
 // check the existence of input
 import React from 'react';
 import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 describe('Todos', () => {
   it('Todos should contain one input', () => {});

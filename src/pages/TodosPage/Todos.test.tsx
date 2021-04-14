97d7a1da18c0fb6441ea4a5e84055690d1e99440
SDen4 <asw325@yandex.ru>
2021-04-14 11:34:21 +0500

    test | check the existence of one input in TodosPage

--- src/pages/TodosPage/Todos.test.tsx
+++ src/pages/TodosPage/Todos.test.tsx
@@ -1,8 +1,16 @@
-// check the existence of input
+// check the existence of one input in TodosPage //
 import React from 'react';
-import { configure } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import Todos from './TodosPage';
+import { TodoForm } from '../../components/TodoForm';
+
+configure({ adapter: new Adapter() });
 
 describe('Todos', () => {
-  it('Todos should contain one input', () => {});
+  it('Todos should contain one input component', () => {
+    const wrapper = shallow(<Todos />);
+
+    expect(wrapper.find(TodoForm)).toHaveLength(1);
+  });
 });

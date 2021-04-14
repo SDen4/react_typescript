2855747498242cefb04c7a631575ffa62303b389
SDen4 <asw325@yandex.ru>
2021-04-14 11:53:54 +0500

    test | check the existence of one TodoList in TodosPage + ref

--- src/pages/TodosPage/TodosPage.test.tsx
+++ src/pages/TodosPage/TodosPage.test.tsx
@@ -1,16 +1,22 @@
-// check the existence of one input in TodosPage //
+// check the existence of one TodoForm and one TodoList in TodosPage //
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Todos from './TodosPage';
+import TodosPage from './TodosPage';
 import { TodoForm } from '../../components/TodoForm';
+import TodoList from '../../components/TodoList';
 
 configure({ adapter: new Adapter() });
+const wrapper = shallow(<TodosPage />);
 
-describe('Todos', () => {
-  it('Todos should contain one input component', () => {
-    const wrapper = shallow(<Todos />);
-
+describe('TodosPage -> Input', () => {
+  it('TodosPage should contain one TodoForm component', () => {
     expect(wrapper.find(TodoForm)).toHaveLength(1);
+  });
+});
+
+describe('TodosPage -> List', () => {
+  it('TodosPage should contain one TodoList component', () => {
+    expect(wrapper.find(TodoList)).toHaveLength(1);
   });
 });

9ce2df6ab41bd29aa77a16ef6cf88cef46eee3a1
SDen4 <asw325@yandex.ru>
2021-04-14 10:52:52 +0500

    yarn add @types/enzyme --dev

--- src/pages/TodosPage/Todos.test.tsx
+++ src/pages/TodosPage/Todos.test.tsx
@@ -1,3 +1,7 @@
 // check the existence of input
-
 import React from 'react';
+import { configure } from 'enzyme';
+
+describe('Todos', () => {
+  it('Todos should contain one input', () => {});
+});
